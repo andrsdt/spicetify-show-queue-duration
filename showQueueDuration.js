e70da977385680cdbf201907ab3801fb492edf2f
Andres Duran <andresdt@pm.me>
2022-10-13 16:16:43 +0200

    fix: css class selector

--- showQueueDuration.js
+++ showQueueDuration.js
@@ -5,34 +5,35 @@
 /// <reference path="../globals.d.ts" />
 
 (function ShowQueueDuration() {
-  // Only run on the queue page
-  if (Spicetify.Platform?.History.location.pathname !== "/queue") {
-    setTimeout(ShowQueueDuration, 1000);
-    return;
-  }
-  const queueText = document
-    .querySelector(".main-type-canon")
-    .textContent.split(" -")[0];
+	const QUEUE_H1_CLASS = ".DG9CsoFIptJhAneKoo_F";
+	// Only run on the queue page
+	if (Spicetify.Platform?.History.location.pathname !== "/queue") {
+		setTimeout(ShowQueueDuration, 1000);
+		return;
+	}
+	const queueText = document
+		.querySelector(QUEUE_H1_CLASS)
+		.textContent.split(" -")[0];
 
-  let nextTracks = Spicetify.Queue.nextTracks.filter(
-    (t) => t.provider === "queue"
-  );
+	let nextTracks = Spicetify.Queue.nextTracks.filter(
+		(t) => t.provider === "queue"
+	);
 
-  const queueDuration = nextTracks
-    .map((t) => parseInt(t.contextTrack.metadata.duration) || 0)
-    .reduce((a, b) => a + b, 0); // Sum up the durations
+	const queueDuration = nextTracks
+		.map((t) => parseInt(t.contextTrack.metadata.duration) || 0)
+		.reduce((a, b) => a + b, 0); // Sum up the durations
 
-  const currentSongProgress = Spicetify.Player.getProgress() || 0;
-  const currentSongDuration = Spicetify.Player.getDuration() || 0;
-  const currentSongRemaining = currentSongDuration - currentSongProgress;
+	const currentSongProgress = Spicetify.Player.getProgress() || 0;
+	const currentSongDuration = Spicetify.Player.getDuration() || 0;
+	const currentSongRemaining = currentSongDuration - currentSongProgress;
 
-  const prettyDuration = new Date(queueDuration + currentSongRemaining)
-    .toGMTString()
-    .split(" ")[4]; // Index of "hh:mm:ss"
+	const prettyDuration = new Date(queueDuration + currentSongRemaining)
+		.toGMTString()
+		.split(" ")[4]; // Index of "hh:mm:ss"
 
-  document.querySelector(".main-type-canon").textContent =
-    queueText + `  -  ${prettyDuration}`;
+	document.querySelector(QUEUE_H1_CLASS).textContent =
+		queueText + `  -  ${prettyDuration}`;
 
-  // Update every second
-  setTimeout(ShowQueueDuration, 1000);
+	// Update every second
+	setTimeout(ShowQueueDuration, 1000);
 })();
