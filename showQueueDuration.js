d0be18a4ce711b87a997135f9474b091dfcf3b1a
Andrés <andresdt@pm.me>
2023-05-01 18:00:33 +0200

    fix: bump for new Spotify versions, add backwards compatibility

--- showQueueDuration.js
+++ showQueueDuration.js
@@ -4,21 +4,29 @@
 
 /// <reference path="../globals.d.ts" />
 
+// HTML class of the queue page header. This may change
+// from one version to another, so I'm adding them as they
+// are added to Spotify. This is to ensure compatibility
+// with older Spotify versions
+const QUEUE_H1_CLASSES = [
+  ".main-type-canon",
+  ".DG9CsoFIptJhAneKoo_F",
+  ".queue-queuePage-header",
+].join(", ");
+
 (function ShowQueueDuration() {
-	const QUEUE_H1_CLASS = ".DG9CsoFIptJhAneKoo_F";
 	// Only run on the queue page
 	if (Spicetify.Platform?.History.location.pathname !== "/queue") {
 		setTimeout(ShowQueueDuration, 1000);
 		return;
 	}
-	const queueText = document
-		.querySelector(QUEUE_H1_CLASS)
-		.textContent.split(" -")[0];
 
-	let nextTracks = Spicetify.Queue.nextTracks.filter(
+	const queueHeader = document.querySelector(QUEUE_H1_CLASSES);
+	const queueText = queueHeader.textContent.split(" -")[0];
+	
+	const nextTracks = Spicetify.Queue.nextTracks.filter(
 		(t) => t.provider === "queue"
 	);
-
 	const queueDuration = nextTracks
 		.map((t) => parseInt(t.contextTrack.metadata.duration) || 0)
 		.reduce((a, b) => a + b, 0); // Sum up the durations
@@ -31,8 +39,7 @@
 		.toGMTString()
 		.split(" ")[4]; // Index of "hh:mm:ss"
 
-	document.querySelector(QUEUE_H1_CLASS).textContent =
-		queueText + `  -  ${prettyDuration}`;
+	queueHeader.textContent = queueText + `  -  ${prettyDuration}`;
 
 	// Update every second
 	setTimeout(ShowQueueDuration, 1000);
